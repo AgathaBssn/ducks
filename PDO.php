2f2fb71961f56d295fd1765355088cc9dd169642
AgathaBssn <144909766+AgathaBssn@users.noreply.github.com>
2025-10-03 17:12:30 +0200

    feat: api file

---
+++ PDO.php
@@ -0,0 +1,24 @@
+﻿<?php
+require_once 'config.php';
+
+/// returns all ducks from the database
+function getAllDucks($pdo) {
+    try {
+        $stmt = $pdo->query("SELECT * FROM ducks");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Erreur : " . $e->getMessage());
+    }
+}
+
+/// returns the number of ducks in the database
+function countAllDucks($pdo) {
+    try {
+        $stmt = $pdo->query("SELECT COUNT(*) as count FROM ducks");
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['count'];
+    } catch (PDOException $e) {
+        die("Erreur : " . $e->getMessage());
+    }
+
+}
