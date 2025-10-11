4125d40e1307d89a3e04e888dd46b5cec0ddae5d
AgathaBssn <agatha.basson@gmail.com>
2025-10-11 17:50:23 +0200

    feat: + beau

--- index.php
+++ index.php
@@ -12,19 +12,13 @@ $ducks = getAllDucks($pdo);
   <title>Accueil</title>
 </head>
 <body>
-  <h1>Bienvenue sur la page d'accueil</h1>
-  <p>Nombre de canards dans la base : <strong><?= htmlspecialchars($count) ?></strong></p>
-
-  <h2>Liste des canards :</h2>
-    <h1>Grille des 100 canards</h1>
+  <h1>Hide and ducks</h1>
   <div class="grid">
       <?php foreach ($ducks as $duck): ?>
           <div class="duck <?= htmlspecialchars($duck['place']) ?>">
               <img src="media/duck.png" alt="Image de canard" class="duck-image">
               <strong><?= htmlspecialchars($duck['name']) ?></strong>
               <br>
-              Place: <?= htmlspecialchars($duck['place']) ?><br>
-              Vu le: <?= htmlspecialchars($duck['last_found']) ?>
           </div>
       <?php endforeach; ?>
   </div>
